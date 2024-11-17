6d5f27fdce05207f951fb775bf983d6f169bc935
Tazer <yaseenelgamal20094@gmail.com>
2024-11-17 20:06:25 +0200

    crash-no-more

--- src/main/java/com/farcr/nomansland/client/particle/FluidFallingParticle.java
+++ src/main/java/com/farcr/nomansland/client/particle/FluidFallingParticle.java
@@ -4,14 +4,16 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.particle.TextureSheetParticle;
-import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 
+import java.util.function.Supplier;
+
 public class FluidFallingParticle extends TextureSheetParticle {
-    private final ParticleOptions flatParticle;
-    public FluidFallingParticle(ClientLevel level, double x, double y, double z, SpriteSet spriteSet, ParticleOptions flatParticle) {
+    private final Supplier<SimpleParticleType> flatParticle;
+    public FluidFallingParticle(ClientLevel level, double x, double y, double z, SpriteSet spriteSet, Supplier<SimpleParticleType> flatParticle) {
         super(level, x, y, z);
         this.setSprite(spriteSet.get(this.random.nextInt(4), 4));
         this.gravity = 0.01F;
@@ -47,7 +49,7 @@ public class FluidFallingParticle extends TextureSheetParticle {
     protected void postMoveUpdate() {
         if (this.onGround) {
             float offset = random.nextInt(-10, 10) * 0.01F;
-            if (flatParticle != null) this.level.addParticle(flatParticle, this.x + (offset * Math.random()), this.y, this.z + (offset * Math.random()), 0.0, 0.0, 0.0);
+            if (flatParticle != null) this.level.addParticle(flatParticle.get(), this.x + (offset * Math.random()), this.y, this.z + (offset * Math.random()), 0.0, 0.0, 0.0);
             float volume = Mth.randomBetween(this.random, 0.3F, 1.0F);
             this.level.playLocalSound(this.x, this.y, this.z, SoundEvents.BEEHIVE_DRIP, SoundSource.BLOCKS, volume, 1.0F, false);
             this.remove();
