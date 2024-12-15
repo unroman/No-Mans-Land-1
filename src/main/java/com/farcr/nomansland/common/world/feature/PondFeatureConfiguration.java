bac05fe8d03d2974a97cbcaaa64df1bcebb92b2f
exoplanetary <kaden_ostendorf@yahoo.com>
2024-12-15 15:53:27 -0700

    added defaults to pond feature

--- src/main/java/com/farcr/nomansland/common/world/feature/PondFeatureConfiguration.java
+++ src/main/java/com/farcr/nomansland/common/world/feature/PondFeatureConfiguration.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.ExtraCodecs;
 import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
@@ -16,13 +17,13 @@ public record PondFeatureConfiguration(IntProvider numPools, IntProvider poolSiz
                     IntProvider.codec(1, 8).fieldOf("num_pools").forGetter(PondFeatureConfiguration::numPools),
                     IntProvider.codec(1, 8).fieldOf("pool_size").forGetter(PondFeatureConfiguration::poolSize),
                     IntProvider.codec(0, 8).fieldOf("pool_eccentricity").forGetter(PondFeatureConfiguration::poolEccentricity),
-                    ExtraCodecs.NON_NEGATIVE_INT.fieldOf("pool_spread").forGetter(PondFeatureConfiguration::poolSpread),
+                    ExtraCodecs.NON_NEGATIVE_INT.fieldOf("pool_spread").orElse(2).forGetter(PondFeatureConfiguration::poolSpread),
                     IntProvider.codec(1,8).fieldOf("pool_depth").forGetter(PondFeatureConfiguration::poolDepth),
-                    Codec.FLOAT.fieldOf("pool_noise").forGetter(PondFeatureConfiguration::poolNoise),
-                    ExtraCodecs.POSITIVE_FLOAT.fieldOf("pool_steepness").forGetter(PondFeatureConfiguration::poolStepth),
-                    BlockStateProvider.CODEC.fieldOf("water_provider").forGetter(PondFeatureConfiguration::waterState),
-                    BlockStateProvider.CODEC.fieldOf("floor_provider").forGetter(PondFeatureConfiguration::floorState),
-                    PondDecorator.CODEC.listOf().fieldOf("decorators").forGetter(PondFeatureConfiguration::decorators)
+                    Codec.FLOAT.fieldOf("pool_noise").orElse(0.0F).forGetter(PondFeatureConfiguration::poolNoise),
+                    ExtraCodecs.POSITIVE_FLOAT.fieldOf("pool_steepness").orElse(1.0F).forGetter(PondFeatureConfiguration::poolStepth),
+                    BlockStateProvider.CODEC.fieldOf("water_provider").orElse(BlockStateProvider.simple(Blocks.WATER)).forGetter(PondFeatureConfiguration::waterState),
+                    BlockStateProvider.CODEC.fieldOf("floor_provider").orElse(BlockStateProvider.simple(Blocks.AIR)).forGetter(PondFeatureConfiguration::floorState),
+                    PondDecorator.CODEC.listOf().fieldOf("decorators").orElse(List.of()).forGetter(PondFeatureConfiguration::decorators)
             ).apply(record, PondFeatureConfiguration::new)
     );
 }
