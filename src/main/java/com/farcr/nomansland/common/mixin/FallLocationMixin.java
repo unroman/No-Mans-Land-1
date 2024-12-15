aa60facf43c426aaf4fe9af0e43a86f1fe672a83
Tazer <yaseenelgamal20094@gmail.com>
2024-12-15 23:41:06 +0200

    fix incorrect wooden scaffolding death message

---
+++ src/main/java/com/farcr/nomansland/common/mixin/FallLocationMixin.java
@@ -0,0 +1,17 @@
+package com.farcr.nomansland.common.mixin;
+
+import com.farcr.nomansland.common.registry.NMLBlocks;
+import net.minecraft.world.damagesource.FallLocation;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FallLocation.class)
+public class FallLocationMixin {
+    @Inject(method = "blockToFallLocation", at = @At("HEAD"), cancellable = true)
+    private static void blockToFallLocation(BlockState state, CallbackInfoReturnable<FallLocation> cir) {
+        if (state.is(NMLBlocks.WOODEN_SCAFFOLDING)) cir.setReturnValue(FallLocation.SCAFFOLDING);
+    }
+}
