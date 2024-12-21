1972a9b996526e4dd919bffb8e0f9f447ac29c4a
exoplanetary <kaden_ostendorf@yahoo.com>
2024-12-21 00:51:48 -0700

    completely refactored boulders so they're not mad stinky anymore (just a little stinky)

--- src/main/java/com/farcr/nomansland/common/world/feature/BoulderFeature.java
+++ src/main/java/com/farcr/nomansland/common/world/feature/BoulderFeature.java
@@ -1,8 +1,10 @@
 package com.farcr.nomansland.common.world.feature;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
@@ -10,6 +12,7 @@ import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class BoulderFeature extends Feature<BoulderFeatureConfiguration> {
@@ -24,65 +27,143 @@ public class BoulderFeature extends Feature<BoulderFeatureConfiguration> {
         RandomSource random = context.random();
         BoulderFeatureConfiguration config = context.config();
 
-        int numBoulders = config.numCubes().sample(random);
-        int cubeSpread = config.cubeSpread().sample(random);
-        int blocksChanged = 0;
+        int numCubes = config.numCubes().sample(random);
 
         ArrayList<BlockPos> stonePos = new ArrayList<>();
 
+        BlockPos.MutableBlockPos cube_origin_pos = origin.mutable();
         BlockPos.MutableBlockPos pos = origin.mutable();
 
-        for (int i = 0; i < numBoulders; i++) {
+        for (int i = 0; i < numCubes; i++) {
+            if (i > 0) {
+                // set cube origin to a random stone block
+                cube_origin_pos.set(stonePos.get(random.nextInt(stonePos.size())));
+            }
+
+            // find cube size, move cube randomly so that at least one block intersects the existing cubes
             int cubeSize = config.cubeSize().sample(random);
-            int cubeSizeY = cubeSize - (i > 0 ? 1 : 0);
-            //int cubeSizeZ = random.nextIntBetweenInclusive(2, 3);
-            int minX = random.nextIntBetweenInclusive(-cubeSpread - cubeSize, cubeSpread);
-            int minZ = random.nextIntBetweenInclusive(-cubeSpread - cubeSize, cubeSpread);
+            if (random.nextFloat() < config.percentLargerCube()) cubeSize++;
+            cube_origin_pos.move(random.nextInt(cubeSize - 1), random.nextInt(cubeSize - 1), random.nextInt(cubeSize - 1));
+            if (cube_origin_pos.getY() < origin.getY() + config.heightMin()) {
+                cube_origin_pos.setY(origin.getY() + config.heightMin());
+            }
+            if (cube_origin_pos.getY() > origin.getY() + config.heightMax() - cubeSize) {
+                cube_origin_pos.setY(origin.getY() + config.heightMax() - cubeSize);
+            }
+
+            // make the cube
+            ArrayList<BlockPos> cubePos = new ArrayList<>();
             for (int x = 0; x < cubeSize; x++) {
-                for (int y = 0; y < cubeSizeY; y++) {
+                for (int y = 0; y < cubeSize; y++) {
                     for (int z = 0; z < cubeSize; z++) {
-                        if ((x > 0 && x < cubeSize - 1) || (z > 0 && z < cubeSize - 1) || y < cubeSizeY - 1 || random.nextFloat() < config.cubeRoundingPercentage()) {
-                            pos.set(origin.offset(x + minX, y, z + minZ));
-                            if (level.getBlockState(pos).is(BlockTags.REPLACEABLE) && !stonePos.contains(pos)) {
-                                while (level.getBlockState(pos.below()).is(BlockTags.REPLACEABLE) && !stonePos.contains(pos.below())) {
-                                    pos.move(Direction.DOWN);
-                                }
-                                //level.setBlock(pos, Blocks.COBBLESTONE.defaultBlockState(), 2);
-                                stonePos.add(pos.immutable());
-                                blocksChanged++;
-                            }
-                        }
+                        pos.set(cube_origin_pos);
+                        pos.move(x, y, z);
+                        cubePos.add(pos.immutable());
                     }
                 }
             }
-        }
 
-        for (int i = 0; i < config.numErosionRounds(); i++) {
-            for (Iterator<BlockPos> it = stonePos.iterator(); it.hasNext();) {
-                BlockPos pos1 = it.next();
-                if (!level.getBlockState(pos1.below()).isAir()) {
-                    int solidCount = 0;
-                    for (Direction direction : Direction.values()) {
-                        if (stonePos.contains(pos1.relative(direction))) {
-                            solidCount += 1;
+            // erode random blocks from the cube with at least 3 adj air blocks
+            int cubeErosion = config.cubeErosion().sample(random);
+            if (config.cubeErosionSizeMultiplier() > 0) cubeErosion = (int) (cubeErosion * config.cubeErosionSizeMultiplier() * cubeSize * cubeSize * cubeSize);
+            int erodedBlocks = 0;
+            BlockPos posToRemove;
+            // I really should refactor this into its own method since it's used 3 times
+            // However I am lazy ¯\_(ツ)_/¯
+            do {
+                Util.shuffle(cubePos, random);
+                posToRemove = null;
+                for (BlockPos cpos : cubePos) {
+                    int directionsWithoutBlocks = 0;
+                    for (Direction d : Direction.values()) {
+                        if (!cubePos.contains(cpos.relative(d))) {
+                            directionsWithoutBlocks++;
+                        } else if (d == Direction.UP) {
+                            // at this point in generation don't erode stuff with blocks above, we're just shaving off the top
+                            directionsWithoutBlocks = -10;
                         }
                     }
-                    if (solidCount < 3 && stonePos.contains(pos1) && random.nextFloat() < config.erosionPercentage()) {
-                        it.remove();
-                        blocksChanged--;
+                    if (directionsWithoutBlocks >= 3) {
+                        posToRemove = cpos;
+                        break;
                     }
                 }
+                if (posToRemove != null) {
+                    cubePos.remove(posToRemove);
+                    erodedBlocks++;
+                }
+            } while (posToRemove != null && erodedBlocks < cubeErosion);
+
+            // add the cube to the final block positions
+            stonePos.addAll(cubePos);
+        }
+
+        // sort stonePos from lowest to highest y for gravity purposes
+        stonePos.sort(Comparator.comparingInt(Vec3i::getY));
+
+        // check all blocks for placeability & apply gravity
+        ArrayList<BlockPos> stonePosPlaced = new ArrayList<>();
+        for (BlockPos pos1 : stonePos) {
+            if (level.getBlockState(pos1).is(BlockTags.REPLACEABLE)) {
+                int i = 0;
+                while (level.getBlockState(pos1.below(i)).is(BlockTags.REPLACEABLE) && !stonePosPlaced.contains(pos1.below(i)) && i < 10) {
+                    i++;
+                }
+                stonePosPlaced.add(pos1.below(i - 1));
             }
         }
 
-        if (blocksChanged < 10) {
+        // extra bit of final erosion
+        BlockPos posToRemove;
+        int erodedBlocks = 0;
+        int erosion = config.extraErosion().sample(random);
+        do {
+            posToRemove = null;
+            for (BlockPos cpos : stonePosPlaced) {
+                int directionsWithoutBlocks = 0;
+                for (Direction d : Direction.values()) {
+                    if (!stonePosPlaced.contains(cpos.relative(d))) {
+                        directionsWithoutBlocks++;
+                    }
+                }
+                if (directionsWithoutBlocks >= 3) {
+                    posToRemove = cpos;
+                    break;
+                }
+            }
+            if (posToRemove != null) {
+                stonePosPlaced.remove(posToRemove);
+                erodedBlocks++;
+            }
+        } while (posToRemove != null && erodedBlocks < erosion);
+
+        // erode sharp edges
+        do {
+            posToRemove = null;
+            for (BlockPos cpos : stonePosPlaced) {
+                int directionsWithoutBlocks = 0;
+                for (Direction d : Direction.values()) {
+                    if (!stonePosPlaced.contains(cpos.relative(d))) {
+                        directionsWithoutBlocks++;
+                    }
+                }
+                if (directionsWithoutBlocks >= 5) {
+                    posToRemove = cpos;
+                    break;
+                }
+            }
+            if (posToRemove != null) {
+                stonePosPlaced.remove(posToRemove);
+            }
+        } while (posToRemove != null);
+
+        if (stonePosPlaced.size() <= config.minimumSize()) {
             return false;
         }
 
-        for (BlockPos pos1 : stonePos) {
-            if (level.getBlockState(pos1).is(BlockTags.REPLACEABLE)) {
-                level.setBlock(pos1, config.blockProvider().getState(random, pos1), 2);
-            }
+        // place all blocks
+        for (BlockPos pos1 : stonePosPlaced) {
+            level.setBlock(pos1, config.blockProvider().getState(random, pos1), 2);
         }
 
         return true;
--- src/main/java/com/farcr/nomansland/common/world/feature/BoulderFeatureConfiguration.java
+++ src/main/java/com/farcr/nomansland/common/world/feature/BoulderFeatureConfiguration.java
@@ -7,15 +7,18 @@ import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
-public record BoulderFeatureConfiguration(IntProvider numCubes, IntProvider cubeSpread, IntProvider cubeSize, float cubeRoundingPercentage, int numErosionRounds, float erosionPercentage, BlockStateProvider blockProvider) implements FeatureConfiguration {
+public record BoulderFeatureConfiguration(IntProvider numCubes, IntProvider cubeSize, float percentLargerCube, IntProvider cubeErosion, float cubeErosionSizeMultiplier, IntProvider extraErosion, int heightMin, int heightMax, int minimumSize, BlockStateProvider blockProvider) implements FeatureConfiguration {
     public static final Codec<BoulderFeatureConfiguration> CODEC = RecordCodecBuilder.create(
             record -> record.group(
                     IntProvider.codec(1, 8).fieldOf("num_cubes").forGetter(BoulderFeatureConfiguration::numCubes),
-                    IntProvider.codec(0, 8).fieldOf("cube_spread").forGetter(BoulderFeatureConfiguration::cubeSpread),
                     IntProvider.codec(1, 8).fieldOf("cube_size").forGetter(BoulderFeatureConfiguration::cubeSize),
-                    ExtraCodecs.POSITIVE_FLOAT.fieldOf("cube_rounding_percentage").forGetter(BoulderFeatureConfiguration::cubeRoundingPercentage),
-                    ExtraCodecs.NON_NEGATIVE_INT.fieldOf("num_erosion_rounds").forGetter(BoulderFeatureConfiguration::numErosionRounds),
-                    ExtraCodecs.POSITIVE_FLOAT.fieldOf("erosion_percentage").forGetter(BoulderFeatureConfiguration::erosionPercentage),
+                    Codec.FLOAT.fieldOf("percent_larger_cube").forGetter(BoulderFeatureConfiguration::percentLargerCube),
+                    IntProvider.codec(0, 8).fieldOf("cube_erosion").forGetter(BoulderFeatureConfiguration::cubeErosion),
+                    Codec.FLOAT.fieldOf("cube_erosion_size_multiplier").forGetter(BoulderFeatureConfiguration::cubeErosionSizeMultiplier),
+                    IntProvider.codec(0, 8).fieldOf("extra_erosion").forGetter(BoulderFeatureConfiguration::extraErosion),
+                    ExtraCodecs.NON_NEGATIVE_INT.fieldOf("height_min").forGetter(BoulderFeatureConfiguration::heightMin),
+                    ExtraCodecs.NON_NEGATIVE_INT.fieldOf("height_max").forGetter(BoulderFeatureConfiguration::heightMax),
+                    ExtraCodecs.NON_NEGATIVE_INT.fieldOf("minimum_size").forGetter(BoulderFeatureConfiguration::minimumSize),
                     BlockStateProvider.CODEC.fieldOf("block_provider").forGetter(BoulderFeatureConfiguration::blockProvider)
             ).apply(record, BoulderFeatureConfiguration::new)
     );
