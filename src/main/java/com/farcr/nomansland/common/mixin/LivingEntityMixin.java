6642f6d199cf0440edfad28f387981c993e53b27
Tazer <yaseenelgamal20094@gmail.com>
2024-12-24 03:43:04 +0200

    fix wooden scaffolding

--- src/main/java/com/farcr/nomansland/common/mixin/LivingEntityMixin.java
+++ src/main/java/com/farcr/nomansland/common/mixin/LivingEntityMixin.java
@@ -1,14 +1,18 @@
 package com.farcr.nomansland.common.mixin;
 
 import com.farcr.nomansland.common.mixinduck.LivingEntityDuck;
+import com.farcr.nomansland.common.registry.NMLBlocks;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
@@ -30,5 +34,10 @@ public abstract class LivingEntityMixin extends EntityMixin implements LivingEnt
         if (this.nomansland$skipDroppingDeathLoot) {
             ci.cancel();
         }
+    }
+
+    @Redirect(method = "handleOnClimbable", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isScaffolding(Lnet/minecraft/world/entity/LivingEntity;)Z"))
+    private boolean handleOnClimbable(BlockState instance, LivingEntity livingEntity) {
+        return instance.is(Blocks.SCAFFOLDING) || instance.is(NMLBlocks.WOODEN_SCAFFOLDING);
     }
 }
