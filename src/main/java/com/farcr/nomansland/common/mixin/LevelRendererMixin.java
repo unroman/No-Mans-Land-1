43b21292242c7a61fe2cb3c8cc2b21260dff4b32
exoplanetary <kaden_ostendorf@yahoo.com>
2025-01-18 15:56:50 -0700

    fixed monster spawners not producing malevolent fire when spawning mobs

---
+++ src/main/java/com/farcr/nomansland/common/mixin/LevelRendererMixin.java
@@ -0,0 +1,21 @@
+package com.farcr.nomansland.common.mixin;
+
+import com.farcr.nomansland.NMLConfig;
+import com.farcr.nomansland.common.registry.NMLParticleTypes;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.core.particles.ParticleOptions;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(LevelRenderer.class)
+public class LevelRendererMixin {
+
+    @ModifyArg(method = "levelEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V", ordinal = 4), index = 0)
+    private ParticleOptions injected(ParticleOptions particle) {
+        if (NMLConfig.MALEVOLENT_SPAWNER.get()) {
+            return NMLParticleTypes.MALEVOLENT_FLAME.get();
+        }
+        return particle;
+    }
+}
