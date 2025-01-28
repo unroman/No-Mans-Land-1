8043e705ec966176b35502301034470c059ed046
Farcr <slender.farris@gmail.com>
2025-01-28 19:03:43 +0100

    tweaks

--- src/main/java/com/farcr/nomansland/common/registry/NMLEffects.java
+++ src/main/java/com/farcr/nomansland/common/registry/NMLEffects.java
@@ -16,5 +16,5 @@ public class NMLEffects {
             DeferredRegister.create(Registries.MOB_EFFECT, NoMansLand.MODID);
 
     public static final DeferredHolder<MobEffect, MobEffect> FLAMMABLE = MOB_EFFECTS.register("flammable",
-            () -> new FlammableEffect(MobEffectCategory.HARMFUL, 4796183));
+            () -> new FlammableEffect(MobEffectCategory.NEUTRAL, 4796183));
 }
