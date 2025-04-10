6dcdbfa3bcffc417da1b7a1dec1ed86f97e42696
Tazer <yaseenelgamal20094@gmail.com>
2025-04-10 19:42:11 +0200

    fix projectile crash and release patch

--- src/main/java/com/farcr/nomansland/common/mixin/ProjectileMixin.java
+++ src/main/java/com/farcr/nomansland/common/mixin/ProjectileMixin.java
@@ -1,5 +1,6 @@
-package com.farcr.nomansland.common.mixin.accessor;
+package com.farcr.nomansland.common.mixin;
 
+import com.farcr.nomansland.common.mixin.EntityMixin;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.Projectile;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,8 +10,6 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 import javax.annotation.Nullable;
 
 @Mixin(Projectile.class)
-public interface ProjectileAccessor {
-
-    @Invoker("getOwner")
-    Entity getOwner();
+public abstract class ProjectileMixin extends EntityMixin {
+    @Shadow @Nullable public abstract Entity getOwner();
 }
