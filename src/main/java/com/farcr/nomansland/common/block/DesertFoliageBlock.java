4bd0ef9d9ca7ea74b3e1eeddac9c9f246c0bf6cd
exoplanetary <kaden_ostendorf@yahoo.com>
2025-05-26 17:15:05 -0600

    fixed bonemeal on succulents + barrel cacti

--- src/main/java/com/farcr/nomansland/common/block/DesertFoliageBlock.java
+++ src/main/java/com/farcr/nomansland/common/block/DesertFoliageBlock.java
@@ -47,7 +47,7 @@ public class DesertFoliageBlock extends BushBlock implements BonemealableBlock {
 
     @Override
     public boolean isBonemealSuccess(Level level, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
-        return blockState.is(NMLBlocks.SHORT_BEACHGRASS) || blockState.is(NMLBlocks.TALL_BEACHGRASS);
+        return true;
     }
 
     @Override
